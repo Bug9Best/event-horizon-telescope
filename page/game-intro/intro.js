5b14e5278b63b9f9d25a2d841484389c59db1a72
Bug9Best <88504995+Bug9Best@users.noreply.github.com>
2022-04-29 07:13:32 +0700

    Optimize Code

--- page/game-intro/intro.js
+++ page/game-intro/intro.js
@@ -1,20 +1,20 @@
 var string = "เราได้รับบทเป็นลิง ที่ได้มาทำงานบนอวกาศ..";
 var array = string.split("");
 var timer;
-var runner = 0;
 
 frameLooper();
 
 function frameLooper() {
-  if (runner == string.length) {
+  let timer = setTimeout('frameLooper()', 70);
+  if (array.length > 0) {
+    document.getElementById("text").innerHTML += array.shift();
+  }
+  else {
+    clearTimeout(timer);
     setTimeout(function () {
       opening();
     }, 1000);
   }
-
-  (array.length > 0) ? document.getElementById("text").innerHTML += array.shift() : clearTimeout(timer);
-  runner++;
-  setTimeout('frameLooper()', 80);
 }
 
 function opening() {
@@ -23,7 +23,7 @@ function opening() {
   let interval = 0;
   let height = screen.height;
   clearInterval(value);
-  setInterval(frame, 1);
+  value = setInterval(frame, 4);
 
   function frame() {
     if (interval < height) {
@@ -34,6 +34,4 @@ function opening() {
       // window.location.href = '../game-lab/lab.html'
     }
   }
-}
-
-
+} 
