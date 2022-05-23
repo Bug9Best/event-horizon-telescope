965359da4376b3da405f56afb8036243c7054809
Bug9Best <88504995+Bug9Best@users.noreply.github.com>
2022-05-23 22:17:54 +0700

    Update Grid

---
+++ page/layout/layout.js
@@ -0,0 +1,8 @@
+let questtion = [
+  "คำถามที่1 : ลิงเลือกซื้อกล้องโทรทรรศน์ใน Solar Shopping Mall ต้องใช้แบบไหนดี",
+]
+
+function showQuestion() {
+  let questtion1 = document.querySelector(".question");
+  questtion1.innerHTML = questtion[0]
+}
