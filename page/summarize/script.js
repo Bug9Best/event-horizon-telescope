5ba612fab955e0bb434c65a252ae16ca6bffd660
Bug9Best <88504995+Bug9Best@users.noreply.github.com>
2022-05-24 15:59:52 +0700

    Merge JS

--- page/summarize/script.js
+++ page/summarize/script.js
@@ -1,3 +1,4 @@
+// Content Part--------------------------------------------------------------------------------------------
 let thVer = [
   "หน้าหลัก",
   "หลุมดำ",
@@ -124,7 +125,7 @@ function thaiVer() {
   title5.innerText = thVer[9]
   subtitle5.innerText = thVer[10]
   title7.innerText = thVer[11]
-  subtitle7.innerText = thVer[12]  + `<br>` + thVer[13]
+  subtitle7.innerText = thVer[12] + `<br>` + thVer[13]
   title8.innerText = thVer[14]
   subtitle8.innerText = thVer[15]
   title9.innerText = thVer[16]
@@ -173,7 +174,7 @@ function engVer() {
   subtitle5.innerText = enVer[10]
   nextButton.innerText = enVer[11]
   title7.innerText = enVer[11]
-  subtitle7.innerText = enVer[12]  + `<br>` + enVer[13]
+  subtitle7.innerText = enVer[12] + `<br>` + enVer[13]
   title8.innerText = enVer[14]
   subtitle8.innerText = enVer[15]
   title9.innerText = enVer[16]
@@ -190,4 +191,137 @@ function engVer() {
   for (let i = 0; i < footerNameTwo.length; i++) {
     footerNameTwo.innerText = enVer[27] + `<br>` + enVer[28]
   }
+}
+
+// Map Part--------------------------------------------------------------------------------------------
+function jcmt() {
+  Swal.fire({
+    title: 'James Clerk Maxwell Telescope',
+    html: '<div style = "color: #5A5F72" style = "color: #5A5F72">สถานที่ตั้ง: ภูเขาไฟโมนาเคอา รัฐฮาวาย USA</div>' + '<div style = "color: gray">ก่อสร้างเสร็จ: 1987</div>',
+    imageUrl: '/assets/Image/JCMT.jpg',
+    imageWidth: 400,
+    imageHeight: 200,
+    imageAlt: 'JCMT',
+    confirmButtonColor: "#FAB400",
+  })
+}
+
+function sma() {
+  Swal.fire({
+    title: 'The Submillimeter Array',
+    html: '<div style = "color: #5A5F72">สถานที่ตั้ง: ด้านบนสุดของ Mauna Kea ในฮาวาย</div>' + '<div style = "color: gray">ก่อสร้างเสร็จ: 2003</div>',
+    imageUrl: '/assets/Image/SMA.jpg',
+    imageWidth: 400,
+    imageHeight: 200,
+    imageAlt: 'SMA',
+    confirmButtonColor: "#FAB400",
+  })
+}
+
+function spt() {
+  Swal.fire({
+    title: 'South Pole Telescope',
+    html: '<div style = "color: #5A5F72">สถานที่ตั้ง: สถานี Amundsen–Scott South Pole <br>ในทวีปแอนตาร์กติกา</div>' + '<div style = "color: gray">ก่อสร้างเสร็จ: 2007</div>',
+    imageUrl: '/assets/Image/SPT.jpg',
+    imageWidth: 400,
+    imageHeight: 200,
+    imageAlt: 'SPT',
+    confirmButtonColor: "#FAB400",
+  })
+}
+
+function alma() {
+  Swal.fire({
+    title: 'Atacama Large Millimeter/Submillimeter Array',
+    html: '<div style = "color: #5A5F72">สถานที่ตั้ง: ทะเลทราย Atacama ทางตอนเหนือของชิลี</div>' + '<div style = "color: gray">ก่อสร้างเสร็จ: 2013</div>',
+    imageUrl: '/assets/Image/ALMA.jpg',
+    imageWidth: 400,
+    imageHeight: 200,
+    imageAlt: 'ALMA',
+    confirmButtonColor: "#FAB400",
+  })
+}
+
+function iram() {
+  Swal.fire({
+    title: 'IRAM 30 meter Telescope',
+    html: '<div style = "color: #5A5F72">สถานที่ตั้ง: เซียร์ราเนวาดา ประเทศสเปน <br>ใกล้ยอดเขา Pico Veleta</div>' + '<div style = "color: gray">ก่อสร้างเสร็จ: 1984</div>',
+    imageUrl: '/assets/Image/IRAM.jpg',
+    imageWidth: 400,
+    imageHeight: 200,
+    imageAlt: 'IRAM',
+    confirmButtonColor: "#FAB400",
+  })
+}
+
+function lmt() {
+  Swal.fire({
+    title: 'The Large Millimeter Telescope',
+    html: '<div style = "color: #5A5F72">สถานที่ตั้ง: บนยอดเขาเซียร์ราเนกราในเม็กซิโก </div>' + '<div style = "color: gray">ก่อสร้างเสร็จ: 2010</div>',
+    imageUrl: '/assets/Image/LMT.jpg',
+    imageWidth: 400,
+    imageHeight: 200,
+    imageAlt: 'LMT',
+    confirmButtonColor: "#FAB400",
+  })
+}
+
+function smt() {
+  Swal.fire({
+    title: 'Arizona Radio Observatory/Submillimeter-wave Astronomy',
+    html: '<div style = "color: #5A5F72">สถานที่ตั้ง: ภูเขา Graham รัฐ Arizona USA </div>' + '<div style = "color: gray">ก่อสร้างเสร็จ: 1993</div>',
+    imageUrl: '/assets/Image/SMT.jpg',
+    imageWidth: 400,
+    imageHeight: 200,
+    imageAlt: 'SMT',
+    confirmButtonColor: "#FAB400",
+  })
+}
+
+function cso() {
+  Swal.fire({
+    title: 'Caltech Submillimeter Observatory',
+    html: '<div style = "color: #5A5F72">สถานที่ตั้ง: ภูเขาไฟ  Maunakea ฮาวาย </div>' + '<div style = "color: gray">ก่อสร้างเสร็จ: 1985</div>',
+    imageUrl: '/assets/Image/CSO.jpg',
+    imageWidth: 400,
+    imageHeight: 200,
+    imageAlt: 'CSO',
+    confirmButtonColor: "#FAB400",
+  })
+}
+
+function green() {
+  Swal.fire({
+    title: 'Greenland Telescope',
+    html: '<div style = "color: #5A5F72">สถานที่ตั้ง: Thule Air Base ตอนเหนือของ Greenland </div>' + '<div style = "color: gray">ก่อสร้างเสร็จ: 2017</div>',
+    imageUrl: '/assets/Image/Greenland.jpg',
+    imageWidth: 400,
+    imageHeight: 200,
+    imageAlt: 'Greenland telescope',
+    confirmButtonColor: "#FAB400",
+  })
+}
+
+function kpno() {
+  Swal.fire({
+    title: 'Kitt Peak National Observatory',
+    html: '<div style = "color: #5A5F72">สถานที่ตั้ง: Kitt Peak, Arizona, US </div>' + '<div style = "color: gray">ก่อสร้างเสร็จ: 1958</div>',
+    imageUrl: '/assets/Image/KPNO.jpg',
+    imageWidth: 400,
+    imageHeight: 200,
+    imageAlt: 'KPNO',
+    confirmButtonColor: "#FAB400",
+  })
+}
+
+function noema() {
+  Swal.fire({
+    title: 'Kitt Peak National Observatory',
+    html: '<div style = "color: #5A5F72">สถานที่ตั้ง: Plateau de Bure ฝรั่งเศส</div>' + '<div style = "color: gray">ก่อสร้างเสร็จ: 2014</div>',
+    imageUrl: '/assets/Image/JCMT.jpg',
+    imageWidth: 400,
+    imageHeight: 200,
+    imageAlt: 'NOEMA',
+    confirmButtonColor: "#FAB400",
+  })
 }
